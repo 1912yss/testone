27ee6470e1273a2801d6621f1f4112de74a199b1
1912yss <764698957@qq.com>
2019-12-09 16:11:21 +0800

    初始化Person

---
+++ src/com/test/Person.java
@@ -0,0 +1,20 @@
+package com.test;/**
+ * ClassName:Person
+ * Package:com.test
+ * Description:
+ *
+ * @date:2019/12/9 16:09
+ * author:15704608924
+ */
+
+/**2019/12/9
+ *
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
